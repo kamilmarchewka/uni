02ac677c36f103b8bf1a6ba646f840326b93512c
Kamil <kamilm2003@gmail.com>
2024-03-19 19:39:47 +0100

    zad_4 (prawie)

--- lab_04/zad4/header.h
+++ lab_04/zad4/header.h
@@ -3,4 +3,54 @@
 
 #include <iostream>
 
+enum STATUS
+{
+    ON,
+    OFF
+};
+enum TRYB
+{
+    ANTENA,
+    TV_KABLOWA
+};
+enum WEJSCIE
+{
+    TV,
+    DVD
+};
+
+class TTelewizor
+{
+    STATUS status; // wl / wyl
+    float glosnosc;
+    float max_glosnosc{20};
+    int maks_liczba_kanalow;
+    int aktualny_kanal;
+    int tryb;    // antena / telewizja kablowa
+    int wejscie; // antena / DVD
+
+public:
+    TTelewizor(STATUS STATUS, int ILOSC_KAN);
+
+    void OnOff() { status == OFF ? status = ON : status = OFF; };
+    bool Volup();
+    bool Voldown();
+    void Chanup();
+    void Chandown();
+    void Drukuj();
+};
+
+class TPilot
+{
+    WEJSCIE tryb;
+
+public:
+    TPilot(WEJSCIE TRYB = TV) : tryb(TRYB){};
+    void Volup(TTelewizor &t) { t.Volup(); };
+    void Voldown(TTelewizor &t) { t.Voldown(); };
+    void OnOff(TTelewizor &t) { t.OnOff(); };
+    void Chanup(TTelewizor &t) { t.Chanup(); };
+    void Chandown(TTelewizor &t) { t.Chandown(); };
+};
+
 #endif
--- lab_04/zad4/source.cpp
+++ lab_04/zad4/source.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 #include "header.h"
 
-// TSilnik
+// TTelewizor
+TTelewizor::TTelewizor(STATUS STATUS, int ILOSC_KAN) : status(STATUS), maks_liczba_kanalow(ILOSC_KAN), glosnosc(5.), aktualny_kanal(2), tryb(TV_KABLOWA), wejscie(TV){};
+
+bool TTelewizor::Volup()
+{
+    if (glosnosc < max_glosnosc)
+    {
+        glosnosc++;
+        return true;
+    }
+    return false;
+};
+bool TTelewizor::Voldown()
+{
+    if (glosnosc > 0)
+    {
+        glosnosc--;
+        return true;
+    }
+    return false;
+};
+void TTelewizor::Chanup()
+{
+    if (aktualny_kanal + 1 > maks_liczba_kanalow)
+        aktualny_kanal = 0;
+    aktualny_kanal++;
+};
+void TTelewizor::Chandown()
+{
+    if (aktualny_kanal - 1 < 0)
+        aktualny_kanal = maks_liczba_kanalow;
+    aktualny_kanal--;
+};
+
+void TTelewizor::Drukuj()
+{
+    std::cout << "Status:       " << status << std::endl;
+    std::cout << "Glosnosc:     " << glosnosc << std::endl;
+    std::cout << "Kanal:        " << aktualny_kanal << std::endl;
+    std::cout << "Tryb:         " << tryb << std::endl;
+    std::cout << "Wejscie:      " << wejscie << std::endl
+              << std::endl;
+};
 
 // TSamochod
