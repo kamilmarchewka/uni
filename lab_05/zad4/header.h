011db89a1f4cd0cbf89fe4fa729bfc3801d980b8
Kamil <kamilm2003@gmail.com>
2024-03-26 15:21:32 +0100

    zad_4

--- lab_05/zad4/header.h
+++ lab_05/zad4/header.h
@@ -3,4 +3,23 @@
 
 #include <iostream>
 
+class Fahrenheit
+{
+public:
+    int temperaturaFahrenheit;
+
+    operator int()
+    {
+        double fToK = (temperaturaFahrenheit + 459.67) * 5 / 9;
+        return static_cast<int>(fToK);
+    }
+
+public:
+    Fahrenheit(double celcius)
+    {
+        double toFahrenheit = 9. / 5. * celcius + 32.;
+        temperaturaFahrenheit = static_cast<int>(toFahrenheit);
+    }
+};
+
 #endif
