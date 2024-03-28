bc045878a53624733af2d8407a84a171be5f1058
Kamil <kamilm2003@gmail.com>
2024-03-28 23:07:16 +0100

    zad_3

--- lab_10/zad3/main.cpp
+++ lab_10/zad3/main.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
-#include <cmath>
+#include <fstream>
+#include <iomanip>
 using namespace std;
-
-template <typename T, int rozmiar>
-class Tablica
-{
-public:
-    Tablica(T values[])
-    {
-        elementyWsk = new T[rozmiar];
-        for (int i = 0; i < rozmiar; i++)
-        {
-            elementyWsk[i] = values[i];
-        }
-    };
-    ~Tablica()
-    {
-        delete[] elementyWsk;
-    }
-
-    void wypiszEl()
-    {
-        for (int i = 0; i < rozmiar; i++)
-            cout << elementyWsk[i] << endl;
-    }
-
-private:
-    T *elementyWsk{nullptr};
-};
 
 int main()
 {
-    Tablica<int, 5> tablicaInt((int[5]){1, 4, 3, 2, 6});
+    string imie, nazwisko;
+    int nrTelefonu;
 
-    Tablica<float, 7> tablicaFloat((float[7]){3.5, 7.86, 2.3, 1.11, 2.9, 4.8876, 2});
+    cout << "Imie: ";
+    cin >> imie;
+    cout << "Nazwisko: ";
+    cin >> nazwisko;
+    cout << "Nr telefonu: ";
+    cin >> nrTelefonu;
 
-    tablicaInt.wypiszEl();
-    cout << endl;
-    tablicaFloat.wypiszEl();
+    fstream plik;
+    plik.open("wizytowka.txt");
 
+    plik << "----------------------------" << endl;
+    plik << "| " << setw(24) << left << imie << " |" << endl;
+    plik << "| " << setw(24) << left << nazwisko << " |" << endl;
+    plik << "| " << setw(24) << left << nrTelefonu << " |" << endl;
+    plik << "----------------------------" << endl;
+
+    plik.close();
     return 0;
 }
